da3a64eeabc4db7d8661abdce689af97a1645638
Kaleo <kaleocheng@gmail.com>
2019-11-08 17:30:22 +0800

    Update vault.sh

--- src/vault.sh
+++ src/vault.sh
@@ -2,6 +2,12 @@
 project=$1
 env=$2
 project_path="$HOME/Wiredcraft/${project}-devops/devops/ansible"
+vault_file="group_vars/all-${env}/vault"
+pyenv="$HOME/Envs/python3/bin/"
+
+if [ ! -f "$project_path/$vault_file" ]; then
+   vault_file="${vault_file}.yml"
+fi
 
 cd "$project_path" || exit
-ANSIBLE_VAULT_PASSWORD_FILE=.vault_password "$HOME/Envs/python3/bin/ansible-vault" decrypt "group_vars/all-${env}/vault" --output -
+ANSIBLE_VAULT_PASSWORD_FILE=.vault_password "$pyenv/ansible-vault" decrypt "$vault_file" --output -
