edaecf7000bff996fa6070506b099ee66b5f1196
Kaleo <kaleocheng@gmail.com>
2020-01-01 15:35:30 +0800

    Updata status and todo command

--- src/status.js
+++ src/status.js
@@ -2,7 +2,7 @@ const current = new Date().toISOString().replace('T', ' ').substr(0, 7)
 module.exports = (pluginContext) => {
   return (search, env = {}) => {
     return new Promise((resolve, reject) => {
-        resolve(`http://localhost:1313/plan/status-${current}/`)
+        resolve(`http://localhost:1313/month/${current}/`)
     })
   }
 }
