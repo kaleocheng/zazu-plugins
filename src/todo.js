18459d3b0ce7c43ee36625007a37bd8a87a40133
Kaleo <kaleocheng@gmail.com>
2020-03-07 11:00:31 +0800

    Fix todo/status date

--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,7 @@
-const current = new Date().toISOString().replace('T', ' ').substr(0, 7)
 module.exports = (pluginContext) => {
   return (search, env = {}) => {
     return new Promise((resolve, reject) => {
+        const current = new Date().toISOString().replace('T', ' ').substr(0, 7)
         resolve(`http://localhost:1313/day/${current}/`)
     })
   }
