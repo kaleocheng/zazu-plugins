6e44ea8eea295d981df27657615c44983a53f90d
Kaleo <kaleocheng@gmail.com>
2021-08-24 11:42:45 +0800

    Update blog-search port

--- src/blog-search.js
+++ src/blog-search.js
@@ -2,7 +2,7 @@ const request = require('request')
 module.exports = (pluginContext) => {
   return (search, env = {}) => {
     return new Promise((resolve, reject) => {
-        let url = `http://127.0.0.1:3000/search/${search}`
+        let url = `http://127.0.0.1:1314/search/${search}`
         request.get({url:url, json:true}, function (e, r, res) {
             resolve(res)
         })
